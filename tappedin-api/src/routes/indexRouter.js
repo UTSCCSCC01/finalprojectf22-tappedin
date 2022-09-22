2179bfa13f788213ed055d5dce8225e7c2d8c12c
Vincent Li <vinchenzoli3@gmail.com>
2022-09-21 23:52:42 -0400

    Made backend ES6 with Babel compiler _ nodemon watch. Also configured linting for code style.

---
+++ tappedin-api/src/routes/indexRouter.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
+
+export default indexRouter;
