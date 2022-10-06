55ea453c354294b177e77076e83b9402b89194b8
terren <terrenjchan@gmail.com>
2022-10-06 14:12:35 -0400

    [TAP-28] Added basic verification, removed most inline styles

--- tappedin-app/src/components/SignUpForm.tsx
+++ tappedin-app/src/components/SignUpForm.tsx
@@ -10,61 +10,85 @@ export default function SignUpForm() {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [type, setType] = useState('');
-    const [errors, setErrors] = useState({});
+    const [errors, setErrors] = useState([]);
 
     const verifyForm = () => {
+        const foundErrors = [];
         // check all are filled
-        // check email format
+        if (!firstName || !lastName || !username || !password || !confirmPassword || !type) {
+            console.log("Missing at least one field.");
+            foundErrors.push("Missing at least one field.");
+        }
         // check password length
+        if (password.length < 6) {
+            console.log("Password is not long enough.");
+            foundErrors.push("Password is not long enough.");
+        }
         // check password == confirm password
         if (password !== confirmPassword){
             console.log("Passwords don't match.");
+            foundErrors.push("Passwords don't match.");
         }
-        return;
+        return foundErrors;
     }
 
     const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
-        verifyForm();
-        console.log(errors);
-        console.log(firstName, lastName, username, email, password, type);
+        const newErrors = verifyForm();
+        console.log(newErrors);
+        // if no errors then send new user to back end
+        if (newErrors.length === 0) {
+            const usr = {
+                firstName: firstName,
+                lastName: lastName,
+                username: username,
+                password: password,
+                email: email,
+                date: new Date(Date.now())
+            };
+            console.log(usr);
+        } else {
+            setErrors(newErrors);
+            return ;
+        }
+
     }
 
 
     return (
-        <div>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
             <h1>Sign Up</h1>
             <div>If you already have an account: Login</div>
             <div>Personal Information:</div>
-            <div style={{display: 'flex', flexDirection: 'column',justifyContent:'left'}}>
-                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
-                    <div style={{display: 'block'}}>
-                        <label htmlFor="firstName" style={{display: 'block'}}>First Name</label>
+            <div>
+                <div>
+                    <div>
+                        <label htmlFor="firstName" >First Name</label>
                         <input type="text" name="firstName" id="firstName" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)}/> 
                     </div>
-                    <div style={{display: 'block'}}>
-                        <label htmlFor="lastName" style={{display: 'block'}}>Last Name</label>
+                    <div>
+                        <label htmlFor="lastName" >Last Name</label>
                         <input type="text" name="lastName" id="lastName" placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                     </div>
                 </div>
                 <div>
-                    <label htmlFor="username" style={{display: 'block'}}>Username</label>
+                    <label htmlFor="username" >Username</label>
                     <input type="text" name="username" id="username" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
                 </div>
                 <div>
-                    <label htmlFor="email" style={{display: 'block'}}>Email</label>
+                    <label htmlFor="email" >Email</label>
                     <input type="email" name="email" id="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div>
-                    <label htmlFor="password" style={{display: 'block'}}>Password</label>
+                    <label htmlFor="password" >Password</label>
                     <input type="password" name="password" id="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
-                    <label htmlFor="confirmPassword" style={{display: 'block'}}>Confirm Password</label>
+                    <label htmlFor="confirmPassword" >Confirm Password</label>
                     <input type="password" name="confirmPassword" id="confirmPassword" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </div>
                 <div>
-                    <label htmlFor="type" style={{display: 'block'}}>Choose Account Type:</label>
+                    <label htmlFor="type" >Choose Account Type:</label>
                     <div>
                         <input type="radio" name="type" id="business" value="business" onChange={(e) => setType(e.target.value)}/> Business
                         <input type="radio" name="type" id="social" value="social" onChange={(e) => setType(e.target.value)}/>Social
