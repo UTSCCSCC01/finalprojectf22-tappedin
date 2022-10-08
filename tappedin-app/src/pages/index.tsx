999a8f72fbfeb99d9d7031d71890f4d981a89bba
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2022-10-07 21:18:27 -0400

    [Tap-3] Implemented hero section using Figma mockup as guidance, created global classes for typography, and buttons, imported Nunito Sans font, configured Tailwind and ESLint

--- tappedin-app/src/pages/index.tsx
+++ tappedin-app/src/pages/index.tsx
@@ -1,10 +1,9 @@
+import Hero from "../sections/Home/Hero";
 
 
 export default function HomePage() 
 {
     return (
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <Hero></Hero>
     );
 }
---
+++ tappedin-app/src/sections/Home/Hero/index.tsx
@@ -0,0 +1,68 @@
+import styles from "./Hero.module.scss";
+
+export default function Hero() 
+{
+    return (
+        <section>
+            <div className="flex flex-row">
+                <div className="px-10">
+                    <div
+                        className={[ styles["customNavbar"], "py-7 px-3" ].join(
+                            " "
+                        )}
+                    >
+                        <div className="columns-3 flex justify-between items-center">
+                            <div>
+                                <h1 className="font-bold">
+                                    Tapped
+                                    <span style={{ color: "#639FAB" }}>
+                                        In.
+                                    </span>
+                                </h1>
+                            </div>
+                            <div className="columns-2">
+                                <h3>About Us</h3>
+                                <h3>FAQ</h3>
+                            </div>
+                            <div>
+                                <h3 className="font-bold">Sign In</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="flex items-center"
+                        style={{ height: "40rem" }}
+                    >
+                        <div>
+                            <h1 className="title pb-5">
+                                {" "}
+                                Lorem ipsum, <br /> consectetur adipiscing,{" "}
+                                <br />
+                                sed do eiusmod.
+                            </h1>
+                            <ul>
+                                <div className="flex items-center pb-3">
+                                    <div className={styles["circle"]}></div>
+                                    <li className="pl-3">
+                                        Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit.
+                                    </li>
+                                </div>
+                                <div className="flex items-center pb-10">
+                                    <div className={styles["circle"]}></div>
+                                    <li className="pl-3">
+                                    sed do eiusmod tempor incididunt.
+                                    </li>
+                                </div>
+                                
+                            </ul>
+                            <button className="button large">Sign Up</button>
+                            <button className="button link">Sign In</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={[ styles["background"] ].join(" ")}></div>
+            </div>
+        </section>
+    );
+}
