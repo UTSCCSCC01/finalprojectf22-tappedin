c64af17645b7fff242690ddf7009a6742cd131ec
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2022-10-19 21:52:05 -0400

    [TAP-4] Created Template for Dashboard

--- tappedin-app/tailwind.config.js
+++ tappedin-app/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/*.{js,ts,jsx,tsx}"
-    "./src/sections/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/*.{js,ts,jsx,tsx}",
+        "./src/sections/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
