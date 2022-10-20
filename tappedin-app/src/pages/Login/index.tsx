2bb40a300560762334abd8b11dc2f77dc499c508
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2022-10-20 17:59:50 -0400

    Added Demo Button

--- tappedin-app/src/pages/Login/index.tsx
+++ tappedin-app/src/pages/Login/index.tsx
@@ -1,9 +1,13 @@
 import LoginForm from "../../components/Login/LoginForm";
 
-export default function LoginPage(){
+export default function LoginPage()
+{
     return (
         <div>
             <LoginForm/>
+            <a href="/Dashboard">
+                <button>Demo Login</button>
+            </a>
         </div>
     );
 }
