0750ac078c3eed52a32b2db193deeb9d603c3b46
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2022-11-01 19:16:04 -0400

    [TAP-21] Added Base Template for Public Profile Implementation

---
+++ tappedin-app/src/pages/PublicProfile/index.tsx
@@ -0,0 +1,44 @@
+import {
+    profileImageContainer,
+    lineBreak
+} from "./PublicProfile.module.scss";
+
+import CoverImage from "../../components/CoverImage";
+
+export default function PublicProfile()
+{
+    return (
+        <div>
+            <CoverImage size="lg"></CoverImage>
+            <div className="container mx-auto px-5 md:px-0">
+                <div className="grid grid-cols-5 mb-8">
+                    <div className="col-span-2">
+                        <div className={ `${profileImageContainer}` }></div>
+                    </div>
+                    <div className="col-span-1 md:col-span-2"></div>
+                    <button className="button col-span-2 md:col-span-1">Connect</button>
+                </div>
+                
+                <div className="grid grid-cols-3 mb-12">
+                    <div className="col-span-3 md:col-span-2">
+                        <h1 className="title">Ben Saobuppha</h1>
+                        <h3>
+                            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet risus feugiat in ante.”
+                        </h3>
+                    </div>
+                </div>
+
+                <div className="mb-12">
+                    {/* TODO: Insert Social Content Here */}
+                </div>
+
+                <div className={ `${lineBreak} mb-12` }></div>
+
+                <div>
+                    <h1 className="font-bold text-4xl mb-6">Info</h1>
+                    {/* TODO: Insert Other Content Here */}
+                </div>
+            </div>
+        </div>
+    );
+}
