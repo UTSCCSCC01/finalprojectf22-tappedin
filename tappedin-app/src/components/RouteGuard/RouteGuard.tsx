0aeee82c12bdf328c0265bc34e1a3b2e8af88b0c
Vincent Li <vinchenzoli3@gmail.com>
2022-11-03 17:51:08 -0400

    [TAP-37] clean up RouteGuard

--- tappedin-app/src/components/RouteGuard/RouteGuard.tsx
+++ tappedin-app/src/components/RouteGuard/RouteGuard.tsx
@@ -1,24 +1,16 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import { FirebaseAuthenticationService } from "../../sdk/services/firebaseAuthenticationService";
-
-//check if you are on the client (browser) or server
 const isBrowser = () => typeof window !== "undefined";
+const localStorageExists = () => typeof localStorage !== "undefined";
 
-function RouteGuard({ router, children }) 
+function RouteGuard({ router, children }): void 
 {
     //Identify authenticated user
-    const isAuthenticated = typeof localStorage === "undefined" ? false : (localStorage.getItem("isLoggedIn") == "true");
-    let unprotectedRoutes = [
+    const isAuthenticated: boolean = localStorageExists() ? (localStorage.getItem("isLoggedIn") == "true") : false;
+    let unprotectedRoutes: Array<String> = [
         "/Login",
         "/Register",
         "/"
     ];
-
-    /**
-   * @var pathIsProtected Checks if path exists in the unprotectedRoutes routes array
-   */
-    let pathIsProtected = unprotectedRoutes.indexOf(router.pathname) === -1;
+    let pathIsProtected: boolean = unprotectedRoutes.indexOf(router.pathname) === -1;
 
     if (isBrowser() && !isAuthenticated && pathIsProtected) 
     {
