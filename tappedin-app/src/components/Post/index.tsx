8a104c1d2eb589a3bec4a59b84d62eb4f88edebd
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2022-11-10 02:16:26 -0500

    [TAP-25] Added Implementation for the Posts inside Dashboard (Feed) and Public Profile

---
+++ tappedin-app/src/components/Post/index.tsx
@@ -0,0 +1,30 @@
+import { postContainer, postImage, postContent } from "./Post.module.scss";
+
+export default function Post({ post }) 
+{
+    return (
+        <div className={`${postContainer}`}>
+            <div
+                className={`${postImage}`}
+                style={
+                    post.imageURL
+                        ? {
+                            backgroundImage: `url(${post.imageURL})`,
+                        }
+                        : {
+                            background:
+                                  "linear-gradient(179.81deg, #BBCDE5 0.16%, rgba(187, 205, 229, 0) 94.83%)",
+                        }
+                }
+            ></div>
+            <div className={`${postContent}`}>
+                <h3 className="is-text-gradient-1 font-bold">{post.title}</h3>
+                <p className="text-sm mb-4 font-semibold">{post.name}</p>
+                <p className="pre-wrap mb-4">{post.content}</p>
+                <div className="">
+                    <p className="text-sm">{post.dateCreated}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
