f2f3f89a3ab038a2464f8d0990dbf82594fcc744
Arib Shaikh <arib.shaikh@mail.utoronto.ca>
2022-11-17 18:13:49 -0500

    [TAP-29] - Added counter for comments functionality

--- tappedin-app/src/components/CommentSectionButton/index.tsx
+++ tappedin-app/src/components/CommentSectionButton/index.tsx
@@ -1,8 +1,19 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FeatherIcon from 'feather-icons-react';
 
-export default function CommentSectionButton ({ id }) {
+interface CommentButtonProps {
+    id: string;
+    commentIDs: Array<String>;
+}
+export default function CommentSectionButton ({ id, commentIDs }): CommentButtonProps {
+    const [ numComments, setNumComments ] = useState(0);
+
+
+    useEffect(() => 
+    {
+        setNumComments(commentIDs.length);
+    }, []);
 
     const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => 
     {
@@ -10,12 +21,17 @@ export default function CommentSectionButton ({ id }) {
         localStorage.setItem("postID", id);
         window.open("/CommentSection", "_self");
     };
+
+    
     
     return (
+        <div className="flex">
             <FeatherIcon
                 icon="message-circle"
                 stroke="#639FAB"
                 onClick={(e) => handleSubmit(e)}
             ></FeatherIcon>
+            <p className="ml-2 mt-1">{ numComments }</p>
+        </div>
     );
 }
--- tappedin-app/src/components/Post/index.tsx
+++ tappedin-app/src/components/Post/index.tsx
@@ -21,6 +21,8 @@ export default function Post({ post })
                 }
             ></div>
             <div className={`${postContent}`}>
+                <LikeButton id={post._id} likeIDs={post.likeIDs}></LikeButton>
+                <CommentSectionButton id={post._id} commentIDs={post.commentIDs}></CommentSectionButton>
                 <h3 className="is-text-gradient-1 font-bold">{post.title}</h3>
                 <p className="text-sm mb-4 font-semibold">{post.name}</p>
                 <p className="pre-wrap mb-4">{post.content}</p>
