b9f1d0a4c220301b853158fd3624f616491cf08e
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2022-11-22 16:02:13 -0500

    Ignore TypeScript Errors

--- tappedin-app/src/components/Accordion.tsx
+++ tappedin-app/src/components/Accordion.tsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Accordion = ({ title, content }) => {
-  const [isActive, setIsActive] = useState(false);
+const Accordion = ({ title, content }) => 
+{
+    const [ isActive, setIsActive ] = useState(false);
 
-  return (
-    <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div style={{ textAlign: 'left'}}>
-          {title}
-          <span style={{float:'right'}}>
-            {isActive ? '˄' : '˅' }
-          </span>
+    return (
+        <div className="accordion-item">
+            <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+                <div style={{ textAlign: "left" }}>
+                    {title}
+                    <span style={{ float:"right" }}>
+                        {isActive ? "˄" : "˅" }
+                    </span>
+                </div>
+                <hr style={{ background: "black", height: "1px", }}/>
+            </div>
+            {isActive && <div style={{ paddingLeft:"1em", color:"GrayText" }}className="accordion-content">{content}</div>}
         </div>
-        <hr style={{ background: 'black', height: '1px', }}/>
-      </div>
-      {isActive && <div style={{ paddingLeft:'1em', color:'GrayText' }}className="accordion-content">{content}</div>}
-    </div>
-  );
+    );
 };
 
 export default Accordion;
