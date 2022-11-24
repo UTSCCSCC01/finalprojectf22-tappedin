1740c722333782ebde349fe69a4b4ee50c96a363
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2022-11-24 15:45:50 -0500

    [TAP-59] Made website responsive, UI refactoring

---
+++ tappedin-app/src/components/SideInformation/SideInformation.tsx
@@ -0,0 +1,34 @@
+import { 
+    leftContainer,
+    contentBox,
+    imageContainer
+} from "./SideInformation.module.scss";
+
+export default function SideInformation()
+{
+    return (
+        <div className={`${leftContainer} text-white col-span-3 hidden md:block`}>
+            <div className="flex flex-col h-full justify-between">
+                <div>
+                    <a href="/"><h2 className="font-semibold mb-12">Tapped<span style={{ color: "#BBCDE5" }}>In.</span></h2></a>
+                            
+                    <h1 className="title mb-2">
+                    Lorem ipsum dolor sit amet.
+                    </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                </div>
+                        
+                <div className={`${contentBox}`}>
+                    <p className="mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className="flex items-center">
+                        <div className={`${imageContainer} mr-4`}></div>
+                        <div>
+                            <p className="font-semibold">Benjamin Saobuppha</p>
+                            <p>Student</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- tappedin-app/src/sections/Home/FAQ/index.tsx
+++ tappedin-app/src/sections/Home/FAQ/index.tsx
@@ -1,32 +1,39 @@
 import React from "react";
-import Accordion from "../../../components/Accordion";
+import Accordion from "../../../components/Accordion/Accordion";
 
 const faqData = [
     {
-        question: "Question 1",
-        answer: "Answer 1"
+        question:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra. Cursus mattis molestie a iaculis at. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Venenatis cras sed felis eget. Aliquet enim tortor at auctor. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Donec ultrices tincidunt arcu non sodales neque sodales ut. ",
     },
     {
-        question: "Question 2",
-        answer: "Answer 2"
+        question:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra. Cursus mattis molestie a iaculis at. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Venenatis cras sed felis eget. Aliquet enim tortor at auctor. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Donec ultrices tincidunt arcu non sodales neque sodales ut. ",
     },
     {
-        question: "Question 3",
-        answer: "Answer 3"
+        question:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra. Cursus mattis molestie a iaculis at. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Venenatis cras sed felis eget. Aliquet enim tortor at auctor. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Donec ultrices tincidunt arcu non sodales neque sodales ut. ",
     },
 ];
 
 export default function FAQ() 
 {
     return (
-        <section className="section">
-            <div className="container mx-auto py-24 xl">
+        <section className="section px-4">
+            <div className="px-4 md:px-10 py-8 xl">
                 <div>
-                    <h1 className="font-bold pb-3">FAQ</h1>                    
+                    <h1 className="font-bold pb-6">FAQ</h1>
                     <div className="accordion">
                         <h2>
                             {faqData.map(({ question, answer }) => (
-                                <Accordion key={question} title={question} content={answer} />
+                                <Accordion
+                                    key={question}
+                                    title={question}
+                                    content={answer}
+                                />
                             ))}
                         </h2>
                     </div>
